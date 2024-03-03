60ec65758d1a4aacf604c989092e4240a06e69f0
Pc <saraevdmitrii@gmail.com>
2024-03-03 17:48:35 +0500

    its work

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10
+
+WORKDIR /app
+
+COPY ./requirements.txt ./
+RUN pip install -r requirements.txt
+COPY . .
+
+# Переходим в директорию /code/
+WORKDIR sber
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- sber/deposit/views.py
+++ sber/deposit/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from datetime import datetime, timedelta
+import calendar
+
+from deposit.serializers import DepositSerializer
+
+
+class DepositCalculationView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = DepositSerializer(data=request.data)
+
+        if serializer.is_valid():
+            data = serializer.validated_data
+            amount = data['amount']
+            rate = data['rate'] / 12
+            periods = data['periods']
+            current_date = data['date']
+
+            total_data = {}
+
+            for _ in range(periods):
+                amount = amount + (amount / 100 * rate)
+                total_data[current_date.strftime('%d.%m.%Y')] = round(
+                    amount, 2
+                )
+                days_in_next_month = get_amount_days_in_month(current_date)
+                current_date = get_next_date(current_date, days_in_next_month)
+            return Response(total_data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
+
+
+def get_amount_days_in_month(date):
+    if date.month == 12:
+        next_month = 1
+    else:
+        next_month = date.month + 1
+    days_in_month = calendar.monthrange(date.year, next_month)[1]
+    return days_in_month
+
+
+def get_next_date(current_date, days):
+    next_date = current_date + timedelta(days=days)
+    return next_date
